81422dce42499f176653118eadfb4170bdcd0dd7
mercade <mercade@a2e4861b-4ae5-0310-b681-bd0aab12f737>
2006-08-27 08:58:06 +0000

    Added the playerrestore command to restore a player from the last known predeath state.
    git-svn-id: https://genesis.tekno.chalmers.se:443/svn/mudlib/trunk@229 a2e4861b-4ae5-0310-b681-bd0aab12f737

--- secure/player_tool.c
+++ secure/player_tool.c
@@ -18,12 +18,14 @@ inherit "/std/object";
 #include <std.h>
 #include <stdproperties.h>
 
-#define PRIVATE		("/private")
-#define PRIVATE_DIR(p)	((p) + PRIVATE)
-#define PLAYERS		("/players")
-#define PLAYERS_DIR(p)	(PRIVATE_DIR(p) + PLAYERS)
-#define BACKUP		("/player_tool")
-#define BACKUP_DIR	(PLAYERS + BACKUP)
+#define PRIVATE		 ("/private")
+#define PRIVATE_DIR(p)	 ((p) + PRIVATE)
+#define PLAYERS		 ("/players")
+#define PLAYERS_DIR(p)	 (PRIVATE_DIR(p) + PLAYERS)
+#define BACKUP		 ("/player_tool")
+#define BACKUP_DIR	 (PLAYERS + BACKUP)
+#define PREDEATH	 (".predeath")
+#define PREDEATH_FILE(p) (PLAYER_FILE(p) + PREDEATH) 
 
 #define CHECK_SO_ACCESS if (!valid_access()) return 0
 
@@ -37,8 +39,9 @@ private static string *alphabet = explode(ALPHABET, "");
 /*
  * Prototypes.
  */
-static nomask int playerput(string str);
 static nomask int playerget(string str);
+static nomask int playerput(string str);
+static nomask int playerrestore(string str);
 
 /*
  * Function name: create_object
@@ -63,8 +66,9 @@ create_object()
 	"commands. Apart from the archwizards and keepers, only people who " +
 	"have been granted special access to this tool can use this tool.",
 	75) + "\nThe commands supported are:\n" +
-"    playerget    - copy a playerfile to your private directory\n" +
-"    playerput    - copy a playerfile back from your private directory\n");
+"    playerget     - copy a playerfile to your private directory\n" +
+"    playerput     - copy a playerfile back from your private directory\n" +
+"    playerrestore - copy a playerfile from its last predeath state\n");
 
     remove_prop(OBJ_I_VALUE);
     remove_prop(OBJ_I_VOLUME);
@@ -89,8 +93,9 @@ init()
 {
     ::init();
 
-    add_action(playerget,    "playerget");
-    add_action(playerput,    "playerput");
+    add_action(playerget,     "playerget");
+    add_action(playerput,     "playerput");
+    add_action(playerrestore, "playerrestore");
 }
 
 /*
@@ -157,6 +162,51 @@ valid_filename(string name)
     }
 
     return (!sizeof(explode(name, "") - alphabet));
+}
+
+/*
+ * Function name: backup_playerfile
+ * Descriptions : Copies an original playerfile into the player tool backup
+ *                directory before overwriting it. It uses sequential numbers
+ *                to avoid overwriting any files.
+ * Arguments    : string
+ * Returns      : int 1/0 - success/failure.
+ */
+nomask static int
+backup_playerfile(string str)
+{
+    string backup;
+    string file;
+    int number = 1;
+
+    backup = BACKUP_DIR + "/" + str + ".o.";
+
+    if (file_size(BACKUP_DIR) != -2)
+    {
+	if (!mkdir(BACKUP_DIR))
+	{
+	    notify_fail("Failed to create backup directory " + BACKUP_DIR + "\n");
+	    return 0;
+        }
+
+	write("Backup directory created " + BACKUP_DIR + "\n");
+    }
+
+    while(file_size(backup + number) > 0)
+    {
+	number++;
+    }
+
+    file = PLAYER_FILE(str) + ".o";
+    backup = backup + number;
+    if (!rename(file, backup))
+    {
+	notify_fail("Failed to write backup file " + backup + "\n");
+	return 0;
+    }
+
+    write("Current playerfile backed up to " + backup + "\n");
+    return 1;
 }
 
 /*
@@ -265,8 +315,7 @@ playerget(string str)
     default:
 	if (!rm(file))
 	{
-	    notify_fail("Failed to remove previous copy of " + RPATH(file) +
-		"\n");
+	    notify_fail("Failed to remove previous copy " + RPATH(file) + "\n");
 	    return 0;
 	}
     }
@@ -296,8 +345,6 @@ playerput(string str)
     string name;
     string file;
     string buffer;
-    string backup;
-    int    number = 1;
 
     CHECK_SO_ACCESS;
 
@@ -358,34 +405,10 @@ playerput(string str)
     /* If there is a file, make a proper backup. */
     if (file_size(file) > 0)
     {
-	backup = BACKUP_DIR + "/" + str + ".o.";
-
-	if (file_size(BACKUP_DIR) != -2)
-	{
-	    if (!mkdir(BACKUP_DIR))
-	    {
-		notify_fail("Failed to create backup directory to backup " +
-		    "the old version of the playerfile.\n");
-		return 0;
-	    }
-
-	    write("Backup directory created: " + BACKUP_DIR + "\n");
-	}
-
-	while(file_size(backup + number) > 0)
-	{
-	    number++;
-	}
-
-	if (!rename(file, (backup + number)))
-	{
-	    notify_fail("Failed to make a backup of the original " +
-		"playerfile in the secured playerfile directory.\n");
-	    return 0;
-	}
-
-	write("Previous playerfile backed up with name " + str + ".o." +
-	    number + " in the backup directory of the player tool.\n");
+        if (!backup_playerfile(str))
+        {
+            return 0;
+        }
     }
 
     if (!write_file(file, buffer))
@@ -395,7 +418,99 @@ playerput(string str)
     }
 
     write("Playerfile from " + capitalize(str) +
-	" copied back to the secured playerfiles directory.\n");
+	" copied back to the playerfiles directory.\n");
+    return 1;
+}
+
+/*
+ * Function name: playerrestore
+ * Description  : This function can be used to transfer/copy a playerfile
+ *                from the private directory of a wizard to the secure
+ *                directory for the playerfiles.
+ * Arguments    : string str - the name of the player whose file to copy.
+ * Returns      : int 1/0 - success/failure.
+ */
+nomask static int
+playerrestore(string str)
+{
+    string name;
+    string file;
+    string buffer;
+
+    CHECK_SO_ACCESS;
+
+    if (!strlen(str))
+    {
+	notify_fail("Syntax: playerrestore <name>\n");
+	return 0;
+    }
+
+    name = this_interactive()->query_real_name();
+    str = lower_case(str);
+
+    if (!valid_filename(str))
+    {
+	notify_fail("Not a valid player name '" + capitalize(str) +
+	    "' since not all characters are letters.\n");
+	return 0;
+    }
+
+    /* The arch-helpers should not be interested in wizards files. Arches
+     * or keepers may do so though.
+     */
+    if (SECURITY->query_wiz_rank(str) &&
+	(SECURITY->query_wiz_rank(name) < WIZ_ARCH))
+    {
+	notify_fail("No reason to playerput a wizards file.\n");
+	return 0;
+    }
+
+    if (objectp(find_player(str)))
+    {
+	notify_fail("The player " + capitalize(str) + " is logged in. It is " +
+	    "useless to playerrestore now.\n");
+	return 0;
+    }
+
+    setuid();
+    seteuid(getuid());
+
+    file = PREDEATH_FILE(str) + ".o";
+    if (file_size(file) <= 0)
+    {
+	notify_fail("File " + RPATH(file) + " not found.\n");
+	return 0;
+    }
+
+    write("Found " + RPATH(file) + " dated " + ctime(file_time(file)) + ".\n");
+
+    /* Since there is no copy-efun, we have to read the playerfile first. */
+    buffer = read_file(file);
+    if ((!stringp(buffer)) ||
+	(!strlen(buffer)))
+    {
+	notify_fail("Failed to read " + RPATH(file) + "\n");
+	return 0;
+    }
+
+    /* If there is a file, make a proper backup. */
+    file = PLAYER_FILE(str) + ".o";
+    if (file_size(file) > 0)
+    {
+        if (!backup_playerfile(str))
+        {
+            return 0;
+        }
+    }
+
+    if (!write_file(file, buffer))
+    {
+	write("Failed to write " + file + "\n");
+	return 1;
+    }
+
+    write("Playerfile from " + capitalize(str) +
+	" copied back to the last known predeath state.\n");
     return 1;
 }
 
