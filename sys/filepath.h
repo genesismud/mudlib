c11b91892b4805a8253a5a6d72c327878736cac7
mercade <mercade@a2e4861b-4ae5-0310-b681-bd0aab12f737>
2008-04-23 20:18:43 +0000

    Added macros FILE_NAME and FILE_PATH.
    git-svn-id: https://genesis.tekno.chalmers.se:443/svn/mudlib/trunk@385 a2e4861b-4ae5-0310-b681-bd0aab12f737

--- sys/filepath.h
+++ sys/filepath.h
@@ -1,9 +1,8 @@
 /*
-  /sys/filepath.h
-
-  Fix function for getting a correct filepath.
-
-*/
+ * /sys/filepath.h
+ *
+ * Fix function for getting a correct filepath.
+ */
 
 #ifndef FPATH_DEF
 #define FPATH_DEF
@@ -18,24 +17,41 @@
  * Example: path = "/d/Genesis/wiz", name = "below/eastend.c"
  *          => "/d/Genesis/wiz/below/eastend.c"
  */
-#define FPATH(path, name) ((string)FPATH_FILENAME->fix_path(path, name))
+#define FPATH(path, name) ((string)FPATH_FILENAME->fix_path((path), (name)))
 
 /*
  * Gives a full path from a tildepath, with '~/' interpreted as the homedir
  * of the wizard 'name'
  */
-#define TPATH(name, tilde) ((string)FPATH_FILENAME->get_tilde_path(name,tilde))
+#define TPATH(name, tilde) ((string)FPATH_FILENAME->get_tilde_path((name), (tilde)))
 
 /*
  * Combines FPATH and TPATH
  *
  * this_player() is assumed to be the wizard '~/'
  */
-#define FTPATH(p, n) FPATH(p, TPATH(this_player()->query_real_name(), n))
+#define FTPATH(path, name) FPATH((path), TPATH(this_player()->query_real_name(), (name)))
 
 /*
  * Reduces a path to its tilde counterpart
  */
 #define RPATH(path) ((string)FPATH_FILENAME->reduce_to_tilde_path(path))
 
+/*
+ * FILE_PATH(path)
+ *
+ * Get the path part of a file name, including the trailing /.
+ * Example: /d/Domain/dir/foo.c -> /d/Domain/dir/
+ */
+#define FILE_PATH(path) (implode(explode((path), "/")[..-2], "/") + "/")
+
+/*
+ * FILE_NAME(path)
+ *
+ * Get the name part of a file name.
+ * Example: /d/Domain/dir/foo.c -> foo.c
+ */
+#define FILE_NAME(path) (explode((path), "/")[-1..][0])
+
+/* No definitions beyond this line. */
 #endif
