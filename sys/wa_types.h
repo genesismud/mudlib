5227680f8878c95b82ad682b36be87a16ff5b8ba
Erik Gävert <erik@gavert.net>
2021-01-15 12:15:52 +0100

    Adjust skill costs and default weapon dull / break / corrode values
    The skill costs are adjusted to be more fair for different weapon
    types. The weapon types are also adjusted to have the same default
    settings for set_likely_corr/dull/break.

--- sys/wa_types.h
+++ sys/wa_types.h
@@ -28,7 +28,7 @@
  * This defines the chance a recoverable weapon or armour has not to be
  * recovered. (/100)
  */
-#define PERCENTAGE_OF_RECOVERY_LOST     0 
+#define PERCENTAGE_OF_RECOVERY_LOST     0
 
 /* Damage types
  *
@@ -137,9 +137,9 @@
 
 #define A_R_FINGER  	TS_RFINGER
 #define A_L_FINGER  	TS_LFINGER
-#define A_FINGERS    	(TS_RFINGER | TS_LFINGER) 
+#define A_FINGERS    	(TS_RFINGER | TS_LFINGER)
 #define A_ANY_FINGER    (-98304 /* -A_FINGERS */)
- 
+
 #define A_R_FOOT    	TS_RFOOT
 #define A_L_FOOT    	TS_LFOOT
 #define A_FEET      	(TS_RFOOT | TS_LFOOT)
@@ -192,7 +192,7 @@
 #define A_UARM_AC   1
 
 #define A_MAX_AC    ({ 100, 100, 100, 100, 100, 100, 100, 100, \
-		       100, 100, 100, 100, 100, 100, 100, 100 })
+                       100, 100, 100, 100, 100, 100, 100, 100 })
 
 
 /*
@@ -207,24 +207,24 @@
 
 /* Weapon types */
 
-#define W_FIRST     0		/* The first weapon index */
+#define W_FIRST     (0)		/* The first weapon index */
 
-#define W_SWORD     W_FIRST + 0 
-#define W_POLEARM   W_FIRST + 1
-#define W_AXE	    W_FIRST + 2
-#define W_KNIFE     W_FIRST + 3
-#define W_CLUB	    W_FIRST + 4
-#define W_MISSILE   W_FIRST + 5
-#define W_JAVELIN   W_FIRST + 6
-#define W_NO_T      7		/* The number of weapons defined */
+#define W_SWORD     (W_FIRST + 0)
+#define W_POLEARM   (W_FIRST + 1)
+#define W_AXE       (W_FIRST + 2)
+#define W_KNIFE     (W_FIRST + 3)
+#define W_CLUB      (W_FIRST + 4)
+#define W_MISSILE   (W_FIRST + 5)
+#define W_JAVELIN   (W_FIRST + 6)
+#define W_NO_T      (7)		/* The number of weapons defined */
 
 /*
  * Drawbacks are arrange for each weapon type ({ dull, corr, break })
  * and types are ({ sword, polearm, axe, knife, club, missile, javelin })
  */
-#define W_DRAWBACKS ({ ({ 4, 10, 4 }), ({ 7, 10, 10 }), ({ 5, 8, 6 }), \
-                        ({ 6, 6, 8 }), ({ 5, 0, 6 }), ({ 3, 1, 12 }), \
-                        ({ 8, 3, 10 }) })
+#define W_DRAWBACKS ({ ({ 5, 5, 7 }), ({ 5, 5, 7 }), ({ 5, 5, 7 }), \
+                       ({ 5, 5, 7 }), ({ 5, 5, 7 }), ({ 5, 5, 7 }), \
+                       ({ 5, 5, 7 }) })
 
 #define W_WEIGHTS_OFFSET ({ 1000, 3000, 2000,  200, 1000,  500, 1500 })
 #define W_WEIGHTS_FACTOR ({   40,   50,   50,   20,   25,   20,   30 })
